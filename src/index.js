c4d92d3e3e4e06976f19a6b6e4130d3c4c93f387
r3dcrosse <mrdavidwayman@gmail.com>
2018-07-12 01:00:06 -0700

    Fixes setInterval not clearing after socket disconnects

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
+
 const str = require('./test');
 const frame1 = require('./frame1');
 const frame2 = require('./frame2');
 
 server.listen(port, () => console.log(`Server listening at port ${port}`));
+
 let cycleNum = 0;
-const frameGenerator = (socket) => setInterval(() => {
+function frameGenerator(socket) {
   if (cycleNum) {
     console.log('############# sending a frame 1');
     socket.emit(frame1);
@@ -18,26 +20,16 @@ const frameGenerator = (socket) => setInterval(() => {
     socket.emit(frame2);
     cycleNum = 1;
   }
-}, 5000);
+}
 
 io.on('connection', (socket) => {
+  const generator = setInterval(() => frameGenerator(socket), 1000);
   const date = new Date(Date.now());
-
-  console.log(`[${date.toUTCString()}]: Something connected`)
-  socket.on('request show', (data) => {
-    socket.broadcast.emit('request show', { show: 'lol' });
-  });
-
-  socket.on('messageType', (data) => {
-    console.log('GOT HEREEEEEEEEEEEEE')
-    console.log(data)
-  });
-
-  frameGenerator(socket);
+  console.log(`[${date.toUTCString()}]: Something connected`);
 
   socket.on('disconnect', () => {
-    console.log('THIS SOCKET DISCONNECTEDDDD')
-    clearInterval(frameGenerator);
+    console.log('THIS SOCKET Disconnected');
+    clearInterval(generator);
     // socket.broadcast.emit('node disconnected', { node: 1 });
   });
 });
