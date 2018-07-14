fbf4c07d44c90996c3d277375ee7412f0fcb101d
r3dcrosse <mrdavidwayman@gmail.com>
2018-07-14 16:35:08 -0700

    Uses a timer independent of each socket connection to get the correct frame to draw

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
+const NanoTimer = require('nanotimer');
 const str = require('./test');
 const frame1 = require('./frame1');
 const frame2 = require('./frame2');
@@ -48,22 +49,25 @@ function getFrame(time) {
   const tiffBlue2 = "010,186,181,000";
   const white = "000,000,000,255";
 
-  frameArray[time % (pixelCount - 1)] = red;
-  // frameArray[time % (pixelCount - 2)] = cyan;
-  // frameArray[time % (pixelCount - 3)] = white;
-  // frameArray[time % (pixelCount - 4)] = tiffBlue2;
-  // frameArray[time % (pixelCount - 5)] = tiffanyBlue;
+  frameArray[time % pixelCount] = red;
+  frameArray[time % (pixelCount - 1)] = cyan;
+  frameArray[time % (pixelCount - 2)] = white;
+  frameArray[time % (pixelCount - 3)] = tiffBlue2;
+  frameArray[time % (pixelCount - 4)] = tiffanyBlue;
 
   return assembleFrame(node, frameArray);
 }
 
-let cycleNum = 0;
+const time = { frame: 0 };
+const timeKeeper = new NanoTimer();
+timeKeeper.setInterval(() => {
+  console.log('UPDATEDING FRAME TO BE', time.frame);
+  time.frame = time.frame + 1;
+}, '', '1000m');
+
 function frameGenerator(socket) {
-  socket.emit(getFrame(cycleNum));
+  socket.emit(getFrame(time.frame));
   // socket.emit(getFrame(Date.now()));
-  // console.log(`sending frame ${cycleNum}`);
-  cycleNum++;
-  // if (cycleNum) {
   //   console.log('############# sending a frame 1');
   //   socket.emit(frame1);
   //   cycleNum = 0;
@@ -75,24 +79,45 @@ function frameGenerator(socket) {
 }
 
 io.on('connection', (socket) => {
-  nodes[numberOfNodes] = Date.now();
-  numberOfNodes++;
+  const ticker = new NanoTimer();
+  // nodes[numberOfNodes] = Date.now();
+  // numberOfNodes++;
 
-  const latencyBetweenNodes = nodes[numberOfNodes - 1] - nodes[numberOfNodes - 2];
-  console.log('LATENCY', latencyBetweenNodes);
+  // const latencyBetweenNodes = nodes[numberOfNodes - 1] - nodes[numberOfNodes - 2];
+  // console.log('LATENCY', latencyBetweenNodes);
 
-  let frameRate = 500;
-  if (latencyBetweenNodes) {
-    frameRate = frameRate - latencyBetweenNodes;
-  }
+  let frameRate = '10m';
+  // if (latencyBetweenNodes) {
+  //   frameRate = frameRate - latencyBetweenNodes;
+  // }
 
-  const generator = setInterval(() => frameGenerator(socket), frameRate);
+  ticker.setInterval(frameGenerator, [socket], frameRate);
   const date = new Date(Date.now());
   console.log(`[${date.toUTCString()}]: Something connected`);
 
+  socket.on('ping', ({ node }) => {
+    // console.log('SENDING BACK ping FOR node', node);
+    socket.emit(`${node}p`);
+  });
+
+  socket.on('pong', ({ node }) => {
+    // console.log('sending pong for node', node);
+    socket.emit(`${node}z`);
+  });
+
+  socket.on('latency', ({ node, latency }) => {
+    // console.log(`latency for node: ${node} | ${latency}ms`);
+  })
+  //
+  // const ping = setInterval(() => {
+  //   console.log('SENDING PING')
+  //   // socket.emit("{1p");
+  // }, 3000);
+
   socket.on('disconnect', () => {
     console.log('THIS SOCKET Disconnected');
-    clearInterval(generator);
+    ticker.clearInterval();
+    // clearInterval(ping);
     // socket.broadcast.emit('node disconnected', { node: 1 });
   });
 });
