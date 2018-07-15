e666f35807b039b1c1e2dc1ea38533e044960827
r3dcrosse <mrdavidwayman@gmail.com>
2018-07-15 01:18:30 -0700

    Updates websocket server, fixes frame rate issue and

--- src/index.js
+++ src/index.js
@@ -7,19 +7,9 @@ const NanoTimer = require('nanotimer');
 const str = require('./test');
 const frame1 = require('./frame1');
 const frame2 = require('./frame2');
+const utils = require('./utils');
 
 server.listen(port, () => console.log(`Server listening at port ${port}`));
-
-function initializePixels(pixelCount) {
-  const pixel = ['000', '000', '000', '000'];
-  const frameArray = [];
-
-  for (let i = 0; i < pixelCount; i++) {
-    frameArray.push([...pixel]);
-  }
-
-  return frameArray;
-}
 
 function assembleFrame(node, frameArray) {
   let frame = `${node}:`;
@@ -35,13 +25,9 @@ function assembleFrame(node, frameArray) {
   return frame;
 }
 
-let numberOfNodes = 0;
-const nodes = {};
-
-function getFrame(time) {
-  const node = 1;
+function getFrame(time, node) {
   const pixelCount = 25;
-  const frameArray = initializePixels(pixelCount);
+  const frameArray = utils.makeAllBlack(pixelCount);
 
   const cyan = "064,224,208,000";
   const red = "150,000,024,000";
@@ -49,11 +35,24 @@ function getFrame(time) {
   const tiffBlue2 = "010,186,181,000";
   const white = "000,000,000,255";
 
-  frameArray[time % pixelCount] = red;
-  frameArray[time % (pixelCount - 1)] = cyan;
-  frameArray[time % (pixelCount - 2)] = white;
-  frameArray[time % (pixelCount - 3)] = tiffBlue2;
-  frameArray[time % (pixelCount - 4)] = tiffanyBlue;
+  // frameArray[time % (pixelCount + 1)] = red;
+  // frameArray[time % (pixelCount - 2)] = white;
+  // frameArray[time % (pixelCount - 3)] = tiffBlue2;
+  // frameArray[time % (pixelCount - 4)] = tiffanyBlue;
+  // frameArray[time % (pixelCount - 6)] = red;
+  // frameArray[time % (pixelCount - 7)] = cyan;
+  // frameArray[time % (pixelCount - 8)] = white;
+  if (node === 1) {
+    frameArray[time % pixelCount] = "000,000,000,010";
+    // frameArray[(time - 1) % (pixelCount)] = cyan;
+    // frameArray[(time - 2) % (pixelCount)] = tiffBlue2;
+    // frameArray[time % (pixelCount - 2)] = "255, 255, 255, 255";
+  } else if (node === 2) {
+    frameArray[time % pixelCount] = "000,000,000,010";
+    // frameArray[(time - 1) % (pixelCount)] = white;
+    // frameArray[(time - 2) % (pixelCount)] = "100,000,100,010";
+    // frameArray[time % (pixelCount - 2)] = tiffBlue2;
+  }
 
   return assembleFrame(node, frameArray);
 }
@@ -61,37 +60,25 @@ function getFrame(time) {
 const time = { frame: 0 };
 const timeKeeper = new NanoTimer();
 timeKeeper.setInterval(() => {
-  console.log('UPDATEDING FRAME TO BE', time.frame);
-  time.frame = time.frame + 1;
-}, '', '1000m');
+  // console.log('UPDATEDING FRAME TO BE', time.frame);
+  if (false && time.frame + 1 > 61) {
+    time.frame = 1;
+  } else {
+    time.frame = time.frame + 1;
+  }
+}, '', '200m');
 
-function frameGenerator(socket) {
-  socket.emit(getFrame(time.frame));
-  // socket.emit(getFrame(Date.now()));
-  //   console.log('############# sending a frame 1');
-  //   socket.emit(frame1);
-  //   cycleNum = 0;
-  // } else {
-  //   console.log('############# sending a frame 2');
-  //   socket.emit(frame2);
-  //   cycleNum = 1;
-  // }
+function frameGenerator(socket, node) {
+  socket.emit(getFrame(time.frame, node));
 }
 
+let nodes = [];
 io.on('connection', (socket) => {
   const ticker = new NanoTimer();
-  // nodes[numberOfNodes] = Date.now();
-  // numberOfNodes++;
+  const tocker = new NanoTimer();
 
-  // const latencyBetweenNodes = nodes[numberOfNodes - 1] - nodes[numberOfNodes - 2];
-  // console.log('LATENCY', latencyBetweenNodes);
+  const websocketStreamRate = '10m';
 
-  let frameRate = '10m';
-  // if (latencyBetweenNodes) {
-  //   frameRate = frameRate - latencyBetweenNodes;
-  // }
-
-  ticker.setInterval(frameGenerator, [socket], frameRate);
   const date = new Date(Date.now());
   console.log(`[${date.toUTCString()}]: Something connected`);
 
@@ -101,23 +88,26 @@ io.on('connection', (socket) => {
   });
 
   socket.on('pong', ({ node }) => {
-    // console.log('sending pong for node', node);
-    socket.emit(`${node}z`);
+    // socket.emit(`${node}z`);
   });
 
-  socket.on('latency', ({ node, latency }) => {
+  socket.on('latency', (msg) => {
     // console.log(`latency for node: ${node} | ${latency}ms`);
-  })
-  //
-  // const ping = setInterval(() => {
-  //   console.log('SENDING PING')
-  //   // socket.emit("{1p");
-  // }, 3000);
+  });
+
+  socket.on('nodeConnected', ({ node }) => {
+    if (nodes.indexOf(socket) === -1) {
+      nodes.push(socket);
+      console.log('THIS NODE CONNECTED:', node);
+      ticker.setInterval(frameGenerator, [socket, node], websocketStreamRate);
+    }
+  });
 
   socket.on('disconnect', () => {
+    if (nodes.indexOf(socket) > -1) {
+      nodes.splice(socket, 1);
+    }
     console.log('THIS SOCKET Disconnected');
     ticker.clearInterval();
-    // clearInterval(ping);
-    // socket.broadcast.emit('node disconnected', { node: 1 });
   });
 });
